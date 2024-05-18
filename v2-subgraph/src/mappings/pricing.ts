3e2d6395c88ffe8f35f4eb832a3078c7413e7eb5
levi0522 <levi0522@outlook.com>
2024-05-19 02:10:30 +0800

    feat:update price

--- v2-subgraph/src/mappings/pricing.ts
+++ v2-subgraph/src/mappings/pricing.ts
@@ -18,6 +18,9 @@ export function getEthPriceInUSD(): BigDecimal {
   // all 3 have been created
   if (daiPair !== null && usdcPair !== null && usdtPair !== null) {
     let totalLiquidityETH = daiPair.reserve1.plus(usdcPair.reserve1).plus(usdtPair.reserve0)
+    if (totalLiquidityETH.equals(ZERO_BD)){
+      return usdcPair.token1Price
+    }
     let daiWeight = daiPair.reserve1.div(totalLiquidityETH)
     let usdcWeight = usdcPair.reserve1.div(totalLiquidityETH)
     let usdtWeight = usdtPair.reserve0.div(totalLiquidityETH)
@@ -28,6 +31,9 @@ export function getEthPriceInUSD(): BigDecimal {
     // dai and USDC have been created
   } else if (daiPair !== null && usdcPair !== null) {
     let totalLiquidityETH = daiPair.reserve1.plus(usdcPair.reserve1)
+    if (totalLiquidityETH.equals(ZERO_BD)){
+      return usdcPair.token1Price
+    }
     let daiWeight = daiPair.reserve1.div(totalLiquidityETH)
     let usdcWeight = usdcPair.reserve1.div(totalLiquidityETH)
     return daiPair.token1Price.times(daiWeight).plus(usdcPair.token1Price.times(usdcWeight))
